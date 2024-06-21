c0bcad9811d2b4151b927ef993c56a2f9ec181b0
Gursimar Sethi <56084997+gursimarsethi@users.noreply.github.com>
2024-06-21 22:20:45 +0530

    trying gemini implementation
    gemini kinda implmented.

--- config.py
+++ config.py
@@ -1,5 +1,19 @@
 import os
+# from dotenv import load_dotenv
+# # Load the .env file
+# load_dotenv()
 
 class Config:
     OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
     SYSTEM_PROMPT = "You are  a medical expert, that will help the user"
+
+class GoogleAIConfig:
+    GOOGLE_AI_API_KEY = os.getenv("GOOGLE_AI_API_KEY")
+
+# Global variables
+class SPEAKER_TYPES:
+  USER = "user"
+  BOT = "bot"
+
+
+initial_prompt = {"role": SPEAKER_TYPES.BOT, "content": "How may I assist you today?"}
